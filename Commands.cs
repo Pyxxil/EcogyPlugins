8fb2724ad6f3aa527619c89b57db5611d3772722
Josh Hill <josh.hill189@gmail.com>
2022-08-05 11:11:39 +1200

    Add Scale Factor

--- Commands.cs
+++ Commands.cs
@@ -85,17 +85,6 @@ namespace Ecogy
             return places;
         }
 
-        // The CommandMethod attribute can be applied to any public  member 
-        // function of any public class.
-        // The function should take no arguments and return nothing.
-        // If the method is an intance member then the enclosing class is 
-        // intantiated for each document. If the member is a static member then
-        // the enclosing class is NOT intantiated.
-        //
-        // NOTE: CommandMethod has overloads where you can provide helpid and
-        // context menu.
-
-        // Modal Command with localized name
         [CommandMethod("Ecogy", "AddGoogleDrive", CommandFlags.Modal)]
         public void AddGoogleDrive()
         {
@@ -143,38 +132,42 @@ namespace Ecogy
             }
         }
 
-        private static void _Import(string path)
+        private static void _Import(string path, double scale)
         {
             var doc = Application.DocumentManager.MdiActiveDocument;
             for (int i = 0; i < PDFPageCount(path); i++)
             {
-                doc.Editor.Command($"-PDFATTACH", path, 1, new Point2d(SpecSheetCount * 11, 0), 1, 0);
+                doc.Editor.Command($"-PDFATTACH", path, 1, new Point2d(SpecSheetCount * 11 * scale, 0), scale, 0);
                 SpecSheetCount++;
             }
         }
 
-        private static void Import(string path)
+        private static void Import(string path, double scale)
         {
             if (File.GetAttributes(path).HasFlag(FileAttributes.Directory))
             {
                 foreach (var file in Directory.GetFiles(path))
                 {
-                    Import(file);
+                    Import(file, scale);
                 }
             }
             else
             {
-                _Import(path);
+                _Import(path, scale);
             }
         }
 
-        // Modal Command with localized name
         [CommandMethod("Ecogy", "ImportSpecSheet", CommandFlags.Modal)]
         public void ImportSpecSheet()
         {
             var doc = Application.DocumentManager.MdiActiveDocument;
             if (doc != null)
             {
+                var scalePrompt = new PromptDoubleOptions("\nAt what scale? ");
+
+                var scaleResponse = doc.Editor.GetDouble(scalePrompt);
+                var scale = scaleResponse.Value;
+
                 var flags = Autodesk.AutoCAD.Windows.OpenFileDialog.OpenFileDialogFlags.DoNotTransferRemoteFiles |
                     Autodesk.AutoCAD.Windows.OpenFileDialog.OpenFileDialogFlags.DefaultIsFolder |
                     Autodesk.AutoCAD.Windows.OpenFileDialog.OpenFileDialogFlags.ForceDefaultFolder |
@@ -188,7 +181,7 @@ namespace Ecogy
                 {
                     foreach (var file in openFileDialog.GetFilenames())
                     {
-                        Import(file);
+                        Import(file, scale);
                     }
                 }
             }
